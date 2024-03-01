cae51d3c594d86dee08eb653a9eea96bd64d94b3
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-03-01 18:31:06 +0500

    Single Responsibility

---
+++ Design-Patterns-Java/src/SOLID/SingleResponsibility/Calculator.java
@@ -0,0 +1,61 @@
+package SOLID.SingleResponsibility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    public int add(int a , int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+
+    // sun using arrayList it is dynamic type
+    public int arrayListSum(ArrayList<Integer> array) {
+        int total = 0;
+        for (int i = 0; i < array.size(); i++) {
+            total = total + i;
+        }
+        return total;
+    }
+
+    // sum using simple static array
+    public int arraySum(int[] array) {
+        int total = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            total = total + array[i];
+        }
+        return total;
+    }
+
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+
+
+        int[] numbers = {1,2,3,4,5};
+        System.out.println("Sum of simple array: " + calculator.arraySum(numbers));
+
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arrayList.add(i);
+        }
+        System.out.println("Sum of arrayList: " + calculator.arrayListSum(arrayList));
+    }
+
+}
